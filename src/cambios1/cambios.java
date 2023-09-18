fefafda27694da3ef93a3781dfa7c28be0e873f6
edgar <edgar@gmail.com>
2023-09-18 07:05:21 -0600

    gola

--- src/cambios1/cambios.java
+++ src/cambios1/cambios.java
@@ -2,7 +2,7 @@ package cambios1;
 
 public class cambios {
 	public static void main(String[] cecylover) {
-		System.out.println("hola");
+		System.out.println("hola me llamo juan pedro y vamos jajajajajjaja");
 		
 	}
 }
